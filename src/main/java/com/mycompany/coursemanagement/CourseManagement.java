45221631f87aa8159413d1b144889ddf767f946e
pqdat <datkhug133@gmail.com>
2024-01-28 23:09:35 +0700

    v1
    edit sương sương

--- src/main/java/com/mycompany/coursemanagement/CourseManagement.java
+++ src/main/java/com/mycompany/coursemanagement/CourseManagement.java
@@ -3,6 +3,6 @@ package com.mycompany.coursemanagement;
 public class CourseManagement {
 
     public static void main(String[] args) {
-        System.out.println("Dit me thang truong tan dat");
+        System.out.println("Võ Anh Hào");
     }
 }
