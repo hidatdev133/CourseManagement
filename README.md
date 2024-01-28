1bd0e4ca2903b846a7a7ac1b26c4aba8b6c81f3b
pqdat <datkhug133@gmail.com>
2024-01-28 22:51:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CourseManagement
+ Quản lý khóa học
---
+++ src/main/java/com/mycompany/coursemanagement/CourseManagement.java
@@ -0,0 +1,8 @@
+package com.mycompany.coursemanagement;
+
+public class CourseManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
