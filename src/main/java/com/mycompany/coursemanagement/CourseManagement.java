b2c741fde2460602c0e5a9cc1ad1221070e39f8b
pqdat <datkhug133@gmail.com>
2024-01-28 23:16:50 +0700

    delete
    1

--- src/main/java/com/mycompany/coursemanagement/CourseManagement.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.coursemanagement;
-
-public class CourseManagement {
-
-    public static void main(String[] args) {
-        System.out.println("Dit me thang pham quoc dat");
-    }
-}
