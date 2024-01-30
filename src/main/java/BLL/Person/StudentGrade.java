1bfcea04d3ebfd6514388dd39be21e0f8bd7baaf
pqdat <datkhug133@gmail.com>
2024-01-30 20:00:06 +0700

    update BLL

--- src/main/java/BLL/Person/StudentGrade.java
+++ src/main/java/BLL/Person/StudentGrade.java
@@ -1,12 +1,42 @@
 package BLL.Person;
 
 import DAL.StudentGrade.StudentGradeDAL;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.List;
 
 public class StudentGrade {
-    private static ArrayList<StudentGrade> stg;
-    private StudentGradeDAL stgDAL;
-    
-    
-    
+
+    private StudentGradeDAL studentGradeDAL;
+
+    public StudentGrade() {
+        this.studentGradeDAL = new StudentGradeDAL();
+    }
+
+    public List readStudentGrade() {
+        return studentGradeDAL.readStudentGrade();
+    }
+
+    public List findStudentByGrade(float grade) {
+        return studentGradeDAL.findStudentByGrade(grade);
+    }
+
+    public int insertStudentGrade(int courseID, int studentID, float grade) {
+        return studentGradeDAL.insertStudentGrade(courseID, studentID, grade);
+    }
+
+    public boolean deleteStudentGrade(int enrollmentID) {
+        return studentGradeDAL.deleteStudentGrade(enrollmentID);
+    }
+
+    public boolean editStudentGrade(int enrollmentID, int courseID, int studentID, float grade) {
+        return studentGradeDAL.editStudentGrade(enrollmentID, courseID, studentID, grade);
+    }
+
+    public List getCourseIDFromStudentGrade(int courseID) throws SQLException {
+        return studentGradeDAL.getCourseIDFromStudentGrade(courseID);
+    }
+
+    public List getStudentIDFromStudentGrade(int studentID) throws SQLException {
+        return studentGradeDAL.getStudentIDFromStudentGrade(studentID);
+    }
 }
